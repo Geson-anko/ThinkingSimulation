354d5ff4a13b615941948402193df481a5878053
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-03-03 18:37:20 +0900

    Added pairs2graph

--- graph_tools/convertion.py
+++ graph_tools/convertion.py
@@ -3,8 +3,8 @@ def adjac2pairs(adajcency_mat:np.ndarray) -> list[tuple[int,int]]:
     """adjacency matrix を各ノードのペアとして返します。"""
     am = adajcency_mat
     num_nodes = len(am)
-    pairs = [(i,j) for i in range(num_nodes) for j in range(num_nodes) if am[i,j]]
+    pairs = [(i,j) for i in range(num_nodes) for j in range(num_nodes) if am[i][j]]
     return pairs
-    
+
 def pairs2graph(pairs:list[tuple[int,int]], directed:bool, num_nodes:int=None):
     pass
