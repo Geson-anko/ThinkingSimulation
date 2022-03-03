3b7832c1aeac9124d43252c7ded8047f55bb531c
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-03-03 18:49:41 +0900

    Added save_graph

---
+++ graph_tools/io.py
@@ -0,0 +1,6 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def save_graph(G:nx.Graph,file_path:str) -> None:
+    nx.draw(G, with_labels=True, font_weight='bold')
+    plt.savefig(file_path)
