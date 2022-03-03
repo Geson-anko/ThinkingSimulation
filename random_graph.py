d703327ceca68fa500220fe670faf8808530c81d
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-03-03 17:58:29 +0900

    Created random generation process of adjacency matrix

--- random_graph.py
+++ random_graph.py
@@ -25,12 +25,54 @@ settingの形式:
         outdir/n{num_node}_p{prob}_di{directed}_sc{self_connection}{suffix}/plot.png, ...
 """
 import numpy as np
-class SC:
-    NO = "no"
-    ALL = "all"
-    ALLOW = "allow"
+from graph_tools.constants import SC
 
-def 
+def random_generate(num_nodes:int, prob:float, directed:bool, sc:str) -> np.ndarray:
+    """
+    結合確率probのランダムグラフのadjacency matrixを生成します。
+    Args:
+        num_nodes: 最大ノード数です。
+        prob: 結合確率です。
+        directed: エッジの方向の有無です。
+        sc: self_connection の略で、自己結合の有無を決定します。
+            "all", "no", "allow" のいずれかです。
+    """
+    assert 0.0 < prob < 1.0
+    if directed:
+        adj_mat = generate_directed(num_nodes,prob)
+    else:
+        adj_mat = generate_undirected(num_nodes, prob)
+    
+    # self connection process
+    indices = np.arange(num_nodes)
+    if sc == SC.ALL:
+        adj_mat[indices,indices] = True
+    elif sc == SC.NO:
+        adj_mat[indices,indices] = False
+    elif sc == SC.ALLOW:
+        pass
+    else:
+        raise ValueError(f"{sc} is invalid self-connection option! please use all, no, or allow.")
+
+    return adj_mat
+
+def generate_directed(num_nodes:int, prob:float) -> np.ndarray:
+    """向きのある接続確率probのランダムグラフのadajcency matricを返します。"""
+    p,q = prob, 1 - prob
+    adj_mat = np.random.choice([True,False],num_nodes**2, replace=True, p=[p,q])
+    return adj_mat
+
+def generate_undirected(num_nodes:int, prob:float) -> np.ndarray:
+    """向きのない接続確率probのランダムグラフのadajcency matricを返します。"""
+    p,q = prob, 1 - prob
+    adj_mat = np.zeros((num_nodes,num_nodes),bool)
+    indices = np.arange(num_nodes)
+    for i in range(num_nodes):
+        TF = np.random.choice([True,False], num_nodes - i, True, p=[p,q])
+        adj_mat[i,indices[i:]] = TF
+        adj_mat[indices[i:],i] = TF
+
+    return adj_mat
 
 
 if __name__ == "__main__":
