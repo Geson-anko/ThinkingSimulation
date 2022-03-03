c445e0b59953a11f9d7c41417c872affbcd04c2f
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-03-03 18:31:09 +0900

    Created adjacency matrix to pairs
    for graph visualization.

---
+++ graph_tools/convertion.py
@@ -0,0 +1,10 @@
+import numpy as np
+def adjac2pairs(adajcency_mat:np.ndarray) -> list[tuple[int,int]]:
+    """adjacency matrix を各ノードのペアとして返します。"""
+    am = adajcency_mat
+    num_nodes = len(am)
+    pairs = [(i,j) for i in range(num_nodes) for j in range(num_nodes) if am[i,j]]
+    return pairs
+    
+def pairs2graph(pairs:list[tuple[int,int]], directed:bool, num_nodes:int=None):
+    pass
