a93ad9f58c8603b27c99a70a73a9baafb80ba1c0
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-03-03 17:34:36 +0900

    added process flow of random graph generation

--- random_graph.py
+++ random_graph.py
@@ -24,7 +24,53 @@ settingの形式:
         グラフは次のような形でフォルダに保存されます。
         outdir/n{num_node}_p{prob}_di{directed}_sc{self_connection}{suffix}/plot.png, ...
 """
+import numpy as np
 class SC:
     NO = "no"
     ALL = "all"
     ALLOW = "allow"
+
+def 
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    from attrdict import AttrDict
+    import json
+    import os
+    from utils import load_setting
+    # arguments
+    parser = ArgumentParser()
+    parser.add_argument("setting_file",type=str)
+    args = parser.parse_args()
+    
+    # load settings
+    setting = load_setting(args.setting_file)
+
+    num_graphs = len(setting.connection_probs)
+    num_nodes = setting.num_nodes
+    connection_probs = setting.connection_probs
+    directed = setting.directed
+    self_connection = setting.self_connection
+    out_par_dir = setting.outdir
+    suffix = setting.suffix
+
+    print("Generating random graphs...")
+    for i in range(num_graphs):
+        p = connection_probs[i]
+
+        # output paths
+        out_dir = f"{out_par_dir}/n{num_nodes}_p{p}_di{directed}_sc{self_connection}{suffix}"
+        if not os.path.isdir(out_dir):
+            os.makedirs(out_dir)
+
+        plots2d_path = f"{out_dir}/2dplots.png"
+        ajace_mat_path = f"{out_dir}/ajacency_mat.npz"
+
+        adjacency_mat = random_generate(num_nodes, p, directed, self_connection)
+        pairs = adjac2pairs(adjacency_mat)
+        G = pairs2graph(pairs, directed, num_nodes=num_nodes)
+        save_graph(G,plots2d_path)
+        np.save(adjacency_mat, ajace_mat_path)
+    print("Finished.")
+
