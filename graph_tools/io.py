876afdcb980146df77c30ea96112fac0a53cc27c
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-03-04 18:37:48 +0900

    added adjacency_mat_paths

--- graph_tools/io.py
+++ graph_tools/io.py
@@ -1,7 +1,17 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import glob
+import os
 
 def save_graph(G:nx.Graph,file_path:str) -> None:
     nx.draw(G, with_labels=True, font_weight='bold')
     plt.savefig(file_path)
     plt.clf()
+
+def get_adjacency_mat_paths(graph_dir:str) -> list[str]:
+    """adjacency matrixを保存しているファイルパスを取得します。
+    graph_dir/**/adjacency_mat.npy 
+    となっていることを想定しています。
+    """
+    path_param = f"{graph_dir}/**/adjacency_mat.npy"
+    return glob.glob(path_param)
